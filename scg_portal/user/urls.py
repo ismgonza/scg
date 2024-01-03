8f8c7672bcd59483c5e7db0f2b3d2396e895e054
Pino835 <jpino@gtila.com>
2024-01-03 11:55:51 -0600

    Cambios permisos cuenta

--- scg_portal/user/urls.py
+++ scg_portal/user/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
     path('login/', views.sign_in, name='login'),
-    path('index/', TemplateView.as_view(template_name="user/index.html"), name="index"),
-    path('client/<str:nombre_cuenta>/', TemplateView.as_view(template_name="user/client.html"), name="client")
+    path('index/<str:nombre_cuenta>/', views.index_view, name="index"),
+    path('client/<str:nombre_cuenta>/', views.client_view, name="client"),
 ]
--- scg_portal/user/views.py
+++ scg_portal/user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 from .forms import UserForm
 from .models import Usuario, Cuenta
 
@@ -25,21 +25,22 @@ def sign_in(request):
                 if usuario.verificar_contrasena(password):
 
                     if usuario.tipo == 'Admin':
-                        return redirect('index')
-                    elif usuario.tipo == 'Cliente':
-                        # Verificar el campo 'cuenta'
+
+                        request.session['user_correo'] = usuario.correo
+                        request.session['user_tipo'] = usuario.tipo
+                        request.session['user_cuenta_nombre'] = usuario.cuenta.nombre
                         cuenta_nombre = usuario.cuenta.nombre
 
-                        try:
-                            # Buscar la cuenta en la base de datos
-                            cuenta = get_object_or_404(Cuenta, nombre=cuenta_nombre)
+                        return HttpResponseRedirect(reverse("index", kwargs={'nombre_cuenta': cuenta_nombre}))
+                    elif usuario.tipo == 'Cliente':
+                        
+                        request.session['user_correo'] = usuario.correo
+                        request.session['user_tipo'] = usuario.tipo
+                        request.session['user_cuenta_nombre'] = usuario.cuenta.nombre
+                        cuenta_nombre = usuario.cuenta.nombre
 
-                            # Realizar la redirección con el nombre de la cuenta
-                            return HttpResponseRedirect(reverse("client", kwargs={'nombre_cuenta': cuenta_nombre}))
-
-                        except Cuenta.DoesNotExist:
-                            messages.error(request, 'La cuenta no existe')
-                            return redirect('login')
+                        return HttpResponseRedirect(reverse("client", kwargs={'nombre_cuenta': cuenta_nombre}))
+                            
                     else:
                         messages.error(request, 'Tipo de usuario no reconocido')
                         # Puedes ajustar la redirección en este caso según tus necesidades
@@ -54,3 +55,44 @@ def sign_in(request):
 
     form = UserForm()
     return render(request, 'user/login.html', {'form': form})
+
+
+def client_view(request, nombre_cuenta):
+    # Recupera la información del usuario de la sesión
+    user_tipo = request.session.get('user_tipo')
+    user_cuenta_nombre = request.session.get('user_cuenta_nombre')
+
+    try:
+        # Buscar la cuenta en la base de datos
+        cuenta = get_object_or_404(Cuenta, nombre=user_cuenta_nombre)
+
+        # Verificar si el usuario tiene el tipo correcto y la cuenta correcta
+        if user_tipo == 'Cliente' and user_cuenta_nombre == nombre_cuenta:
+            # Tu lógica de vista aquí
+            return render(request, 'user/client.html', {'nombre_cuenta': nombre_cuenta})
+        else:
+            return HttpResponseForbidden("No tienes permiso para acceder a esta página.")
+    
+    except Cuenta.DoesNotExist:
+        messages.error(request, 'La cuenta no existe')
+        return redirect('login')
+    
+def index_view(request, nombre_cuenta):
+    # Recupera la información del usuario de la sesión
+    user_tipo = request.session.get('user_tipo')
+    user_cuenta_nombre = request.session.get('user_cuenta_nombre')
+
+    try:
+        # Buscar la cuenta en la base de datos
+        cuenta = get_object_or_404(Cuenta, nombre=user_cuenta_nombre)
+
+        # Verificar si el usuario tiene el tipo correcto y la cuenta correcta
+        if user_tipo == 'Admin' and user_cuenta_nombre == nombre_cuenta:
+            # Tu lógica de vista aquí
+            return render(request, 'user/index.html', {'nombre_cuenta': nombre_cuenta})
+        else:
+            return HttpResponseForbidden("No tienes permiso para acceder a esta página.")
+    
+    except Cuenta.DoesNotExist:
+        messages.error(request, 'La cuenta no existe')
+        return redirect('login')
